b4508c3a6767a50fa725c206fb32bad8d09e0d98
Andi <popandi04@gmail.com>
2014-03-15 23:11:43 +0200

    Clasa Map

---
+++ SnakeGame/SnakeGame/Model/Map.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.Model
+{
+    class Map
+    {
+        int cellSize;
+        int mapRows;
+        int mapColumns;
+        List<MapCell> mapCells;
+
+        public Map(int cellSize, int mapRows, int mapColumns)
+        {
+            this.cellSize = cellSize;
+            this.mapRows = mapRows;
+            this.mapColumns = mapColumns;
+            mapCells = new List<MapCell>();
+        }
+        public void addMapCell(MapCell newMapCell)
+        {
+            mapCells.Add(newMapCell);
+        }
+    }
+}
