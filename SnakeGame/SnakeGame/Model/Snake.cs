abf28659c6d876b776c2174c6e75f51bbad1f238
ROTARIUANAMARIA <anamaria.anemari@yahoo.com>
2014-03-16 20:43:34 +0200

    A

---
+++ SnakeGame/SnakeGame/Model/Snake.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+
+
+namespace Player
+{
+    class Snake
+    {
+
+      private  Queue<SnakePart> snake = new Queue<SnakePart>();
+      private int direction ; // Down = 0, Left = 1, Right = 2, Up = 3
+
+      public int Direction
+      {
+          get { return direction; }
+          set { direction = value; }
+      }
+      private int step;
+
+      public int Step
+      {
+          get { return step; }
+          set { step = value; }
+      }
+      public Snake(int direction, SnakePart first,int step)
+      {
+          direction = 0;
+          snake.Enqueue(first);
+          step = 0;
+      }
+      //private void StartGame()
+      //{
+      //    direction = 0;
+      //    snake.Clear();
+      //    SnakePart head = new SnakePart();
+      //    head.X = 20;
+      //    head.Y = 5;
+      //    snake.Enqueue(head);
+         
+      //}
+      public void addPart(SnakePart part)
+      {
+          snake.Enqueue(part);
+
+      }
+      public void removePart()
+      {
+          snake.Dequeue();
+
+      }
+   
+    }
+}
---
+++ SnakeGame/SnakeGame/Model/SnakePart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Player
+{
+    class SnakePart
+    {
+        public int X ; 
+        public int Y; 
+        public int getX()
+        {
+        return X;
+        }
+        public int getY()
+        {
+            return Y;
+        }
+        public void setX(int X1)
+        {
+            this.X = X1 ;
+        }
+        public void setY(int Y1)
+        {
+            this.Y = Y1;
+        }
+        // Constructor
+        public SnakePart()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
