e08ec3d3d03482c609d7ac0c1c97d72b3a7e31ac
Andi <popandi04@gmail.com>
2014-03-18 13:56:29 +0200

    Clasa Game v1

---
+++ SnakeGame/SnakeGame/Model/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame.Model
+{
+    class Game
+    {
+        private Level level;
+        private Snake snake;
+        private Player player;
+        private SpeedController speedController;
+
+        public Game(Level level, Snake snake, Player player, SpeedController speedController)
+        {
+            this.level = level;
+            this.snake = snake;
+            this.player = player;
+            this.speedController = speedController;
+        }
+
+    }
+}
